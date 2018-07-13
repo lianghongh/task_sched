878d4f890b0665a78835f7d824a364a725a81aae
lianghongh <lianghongh@126.com>
2018-07-13 12:26:48 +0800

    添加画图程序，删除graph相关程序

--- main/main.py
+++ main/main.py
@@ -1,11 +1,4 @@
-from graph import tgff_tools
 
-if __name__=='__main__':
-    path='/home/lianghong/schedule/example.tgff'
-    graph=tgff_tools.read_graph(path)[0]
-    pe=tgff_tools.read_pe(path)[0]
-    arc=tgff_tools.read_arc(path)[0]
-
-    tgff_tools.build_graph(graph,arc,pe)
-    graph.update_rank_u()
-    print(graph)
+if __name__=="__main__":
+    a=[[1,2],[3,4]]
+    print(list(zip(*a)))
